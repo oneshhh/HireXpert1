20146a62ab81618643a6e1bd7ec4167c09f1ac5c
oneshhh <vanshu2004sabharwal@gmial.com>
2025-09-25 13:54:34 +0530

    fixes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ async function sendInterviewEmail(to, interviewId, title, date, time) {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 587,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
